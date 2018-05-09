7baae94fa1955ff67640a97a8ff769d831fc60d6
mf01271 <aguilerafernanda3@gmail.com>
2018-05-08 20:24:33 -0400

    Estoy agregando el test, hay que cubrir el 80% de código

---
+++ NetMarket.Tests/ProductoTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetMarketData.Domain.Services;
+
+namespace NetMarket.Tests
+{
+    [TestClass]
+    public class ProductoTest
+    {
+        ProductoService productoService = new ProductoService();
+
+        [TestMethod]
+        public void ObtenerProductoExisteTest()
+        {
+            productoService.Obtenerproducto(1);
+            Assert.IsNotNull(producto);
+        }
+        [TestMethod]
+        public void ObtenerProductoNoExisteTest()
+        {
+            productoService.Obtenerproducto(1);
+            Assert.IsNull(producto);
+        }
+    }
+}
